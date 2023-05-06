5fd4c95db9c9949abb1036b1a5132233808298e9
Amir Khan <khanamir8589@gmail.com>
2023-05-06 18:28:55 +0530

    refactored code

---
+++ 15_pattern.js
@@ -0,0 +1,38 @@
+/*
+
+
+     *
+    * *
+   *   *
+  *     *
+ *       *
+  *     *
+   *   *
+    * *
+     *
+         
+         
+*/
+
+function pattern(n) {
+  let result = "";
+
+  for (let row = 1; row <= n * 2 - 1; row++) {
+    for (let col = 1; col <= n * 2 - 1; col++) {
+      if (row <= n) {
+        col === n - row + 1 || col === n + row - 1
+          ? (result += "*")
+          : (result += " ");
+      } else {
+        col === row - n + 1 || col === n * 2 - row + n - 1
+          ? (result += "*")
+          : (result += " ");
+      }
+    }
+
+    if (row !== n * 2 - 1) result += "\n";
+  }
+  return result;
+}
+
+console.log(pattern(5));
--- 18_pattern.js
+++ 18_pattern.js
@@ -15,18 +15,21 @@
 
 function pattern(n) {
   let result = "";
-  for (let row = 1; row <= n; row++) {
+  for (let row = 1; row <= n * 2; row++) {
     for (let col = 1; col <= n * 2; col++) {
-      col <= n - row + 1 || col >= row + n ? (result += "*") : (result += " ");
+      if (row <= n) {
+        col <= n - row + 1 || col >= row + n
+          ? (result += "*")
+          : (result += " ");
+      } else {
+        col <= row - n || col >= n * 2 - row + n + 1
+          ? (result += "*")
+          : (result += " ");
+      }
     }
-    result += "\n";
+    if (row !== n * 2) result += "\n";
   }
-  for (let row = 1; row <= n; row++) {
-    for (let col = 1; col <= n * 2; col++) {
-      col <= row || col >= n * 2 - row + 1 ? (result += "*") : (result += " ");
-    }
-    if (row !== n) result += "\n";
-  }
+
   return result;
 }
 
