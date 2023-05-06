41e041cb10f6d8b126df1ebea8145aa878f936b3
Amir Khan <khanamir8589@gmail.com>
2023-05-06 17:54:46 +0530

    refactored 13_pattern

--- 13_pattern.js
+++ 13_pattern.js
@@ -11,24 +11,10 @@ function pattern(n) {
   let result = "";
 
   for (let row = 1; row <= n; row++) {
-    for (let space = 1; space <= n - row; space++) {
-      result += " ";
-    }
-
-    const printCol = row === n ? n * 2 - 1 : 1;
-    for (let col = 1; col <= printCol; col++) {
-      result += "*";
-    }
-
-    const middleSpaces = row * 2 - n + 2;
-    for (let space = 1; space <= middleSpaces; space++) {
-      result += " ";
-    }
-
-    for (let col = 1; col <= 1; col++) {
-      if (row != n) {
-        row !== 1 && (result += "*");
-      }
+    for (let col = 1; col <= n * 2 - 1; col++) {
+      row === n || col === n - row + 1 || col === n + row - 1
+        ? (result += "*")
+        : (result += " ");
     }
 
     if (row !== n) result += "\n";
